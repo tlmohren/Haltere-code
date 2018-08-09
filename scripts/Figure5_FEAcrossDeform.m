dca7a99eae1094025e54d43da2c0355919756106
tlmohren <tlmohren@uw.edu>
2018-08-09 16:10:24 -0700

    new division of figures

--- scripts/Figure5_FEAcrossDeform.m
+++ scripts/Figure5_FEAcrossDeform.m
@@ -65,17 +65,19 @@ end
 
 %% deformation in angles 
 
-fig1 = figure();
-    width = 2;     % Width in inches,   find column width in paper 
+%% deformation in angles 
+fig4 = figure();
+    width = 3;     % Width in inches,   find column width in paper 
     height = 3;    % Height in inches
-    set(fig1, 'Position', [fig1.Position(1:2) width*100, height*100]); %<- Set size
+    set(fig4, 'Position', [fig4.Position(1:2) width*100, height*100]); %<- Set size
 
+deformLabels = {'$\Delta \phi$','$\Delta \theta$','$\Delta \gamma$'};
+legend_entries = {'ellipsoid hor', 'ellipsoid ver' };
 len = 101;
 start = 35;
 It = start:(start+len-1);
 t_plot = (0:len-1)*0.001;
 
-deformLabels = {'$\Delta \phi$','$\Delta \theta$','$\Delta \gamma$'};
 
 axOpts_dphi= {'XGrid','On','XLim',[0,t_plot(end)],'XTick',[0:0.05:t_plot(end)],'XTickLabel',{'','',''} ,...
                'YLim',[-0.02,0.02]}; 
@@ -83,99 +85,42 @@ axOpts_dtheta= {'XGrid','On','XLim',[0,t_plot(end)],'XTick',[0:0.05:t_plot(end)]
                'YLim',[-1,1]*1e-3}; 
 axOpts_dgamma= {'XGrid','On','XLim',[0,t_plot(end)],'XTick',[0:0.05:t_plot(end)] ,...
                'YLim',[-1,1]*1e-3}; 
+           
+for k = 1
+    for j = [1,2]
+        jk = 2*(k-1)+j
+        subplot(3,2, j ); hold on 
+            p1 = plot( t_plot, FEA(jk).yAngle(It) );
+            ylabel( deformLabels{1} );
+            ax = gca();
+            set(ax,axOpts_dphi{:})
 
-for j = [1]
-    subplot(3,1,1); hold on 
-        p1 = plot( t_plot, FEA(j).yAngle(It) );
-        ylabel( deformLabels{1} );
-        ax = gca();
-        set(ax,axOpts_dphi{:})
-        
-    subplot(312); hold on 
-        p1 = plot( t_plot, FEA(j).zAngle(It) );
-        ylabel( deformLabels{2} );
-        ax = gca();
-        set(ax,axOpts_dtheta{:})
-%         
-    subplot(313); hold on 
-        p1 = plot( t_plot, FEA(j).twistAngle(It) );
-        xlabel('Time (s)'); ylabel( deformLabels{3} );
-        ax = gca();
-        set(ax,axOpts_dgamma{:})
+        subplot(3,2, j+2 ); hold on 
+            p1 = plot( t_plot, FEA(jk).zAngle(It) );
+            ylabel( deformLabels{2} );
+            ax = gca();
+            set(ax,axOpts_dtheta{:})
+
+        subplot(3,2, j+4 ); hold on 
+            p1 = plot( t_plot, FEA(jk).twistAngle(It) );
+            xlabel('Time (s)'); ylabel( deformLabels{3} );
+            ax = gca();
+            set(ax,axOpts_dgamma{:})
+    end
 end
 
 %% Setting paper size for saving 
-
 set(gca, 'LooseInset', get(gca(), 'TightInset')); % remove whitespace around figure
-set(fig1,'InvertHardcopy','on');
-set(fig1,'PaperUnits', 'inches');
-papersize = get(fig1, 'PaperSize');
+set(fig4,'InvertHardcopy','on');
+set(fig4,'PaperUnits', 'inches');
+papersize = get(fig4, 'PaperSize');
 left = (papersize(1)- width)/2;
 bottom = (papersize(2)- height)/2;
 myfiguresize = [left, bottom, width, height];
-set(fig1, 'PaperPosition', myfiguresize);
-print(fig1, ['figs' filesep 'Figure5_deformAnglePlotOM0' ], '-dpng', '-r600');
+set(fig4, 'PaperPosition', myfiguresize);
+
+print(fig4, ['figs' filesep 'Figure3_deformCross' ], '-dpng', '-r600');
 stupid_ratio = 15/16;
 myfiguresize = [left, bottom, width*stupid_ratio, height*stupid_ratio];
-set(fig1, 'PaperPosition', myfiguresize);
-print(fig1, ['figs' filesep 'Figure5_deformAnglePlotOM0'], '-dsvg', '-r600');
-
-
-%%
-
-
-
-fig2 = figure();
-    width = 2;     % Width in inches,   find column width in paper 
-    height = 3;    % Height in inches
-    set(fig2, 'Position', [fig2.Position(1:2) width*100, height*100]); %<- Set size
-
-len = 101;
-start = 35;
-It = start:(start+len-1);
-t_plot = (0:len-1)*0.001;
-
-deformLabels = {'$\Delta \phi$','$\Delta \theta$','$\Delta \gamma$'};
-
-axOpts_dphi= {'XGrid','On','XLim',[0,t_plot(end)],'XTick',[0:0.05:t_plot(end)],'XTickLabel',{'','',''} ,...
-               'YLim',[-0.02,0.02]}; 
-axOpts_dtheta= {'XGrid','On','XLim',[0,t_plot(end)],'XTick',[0:0.05:t_plot(end)],'XTickLabel',{'','',''} ,...
-               'YLim',[-1,1]*1e-3}; 
-axOpts_dgamma= {'XGrid','On','XLim',[0,t_plot(end)],'XTick',[0:0.05:t_plot(end)] ,...
-               'YLim',[-1,1]*1e-3}; 
-
-for j = 2
-    subplot(3,1,1); hold on 
-        p1 = plot( t_plot, FEA(j).yAngle(It) );
-        ylabel( deformLabels{1} );
-        ax = gca();
-        set(ax,axOpts_dphi{:})
-        
-    subplot(312); hold on 
-        p1 = plot( t_plot, FEA(j).zAngle(It) );
-        ylabel( deformLabels{2} );
-        ax = gca();
-        set(ax,axOpts_dtheta{:})
-%         
-    subplot(313); hold on 
-        p1 = plot( t_plot, FEA(j).twistAngle(It) );
-        xlabel('Time (s)'); ylabel( deformLabels{3} );
-        ax = gca();
-        set(ax,axOpts_dgamma{:})
-end
-
-%% Setting paper size for saving 
-
-set(gca, 'LooseInset', get(gca(), 'TightInset')); % remove whitespace around figure
-set(fig2,'InvertHardcopy','on');
-set(fig2,'PaperUnits', 'inches');
-papersize = get(fig2, 'PaperSize');
-left = (papersize(1)- width)/2;
-bottom = (papersize(2)- height)/2;
-myfiguresize = [left, bottom, width, height];
-set(fig2, 'PaperPosition', myfiguresize);
-print(fig2, ['figs' filesep 'Figure5_deformAnglePlotOM10' ], '-dpng', '-r600');
-stupid_ratio = 15/16;
-myfiguresize = [left, bottom, width*stupid_ratio, height*stupid_ratio];
-set(fig2, 'PaperPosition', myfiguresize);
-print(fig2, ['figs' filesep 'Figure5_deformAnglePlotOM10'], '-dsvg', '-r600');
+set(fig4, 'PaperPosition', myfiguresize);
+print(fig4, ['figs' filesep 'Figure3_deformCross'], '-dsvg', '-r600');
