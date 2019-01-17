6197a7f647735efc1799e725540c23210d39bc19
tlmohren <tlmohren@uw.edu>
2019-01-17 15:09:54 -0800

    clearing of old files

--- scripts/old/functions/config.m
+++
@@ -1,8 +0,0 @@
-
-fflap = 25
-res_factor = 9;
-
-M = 1;
-r = 0.05
-R=1
-nu = 0.66
--- scripts/old/functions/diffDepVar.m
+++
@@ -1,5 +0,0 @@
-function res = diffDepVar(fun,depVar)
-    syms xx
-    res = diff(subs(fun,depVar,xx),xx);
-    res = subs(res,xx,depVar);
-end
